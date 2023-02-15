9f29fb0357acdadd3b9c8e5e02b84e57810b472f
Walker Le <walker.le88@gmail.com>
2023-02-15 10:34:27 -0700

    commit tree 80b7987ca45aee1fc9af79cd79bd16d9cd7bf8bd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,33 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    games = Game.all
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    # game.to_json()
+    # game.to_json(include: :reviews)
+    # game.to_json(include: { reviews: { include: :user } })
+    game.to_json(
+      only: [:id, :title, :genre, :price],
+      include: {
+        reviews: {
+          only: [:comment, :score],
+          include: {
+            user: {
+              only: [:name]
+            }
+          }
+        }
+      }
+    )
   end
 
 end
